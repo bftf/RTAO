79770fb746f43884ed8e4263dabcd2385e775778
bftf <f.demoullin@gmail.com>
2019-10-28 15:33:46 -0700

    updated file paths

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ int main()
   const std::string model_name = "teapot";
   const std::string obj_path_addition = "/teapot/teapot.obj";
 
-  // const std::string model_path = "/home/francois/Documents/RayTracing/models/Sponza/models/sponza.obj";
-  // const std::string out_ply_path = "/home/francois/Documents/RayTracing/CWBVH/build_make/ply_files/sponza.ply";
+  const std::string model_name = "sponza";
+  const std::string obj_path_addition = "sponza.obj";
 
-  // const std::string model_path = "/home/francois/Documents/RayTracing/models/Dragon/dragon.obj";
-  // const std::string out_ply_path = "/home/francois/Documents/RayTracing/CWBVH/build_make/ply_files/dragon.ply";
+  const std::string model_name = "dragon";
+  const std::string obj_path_addition = "Dragon/dragon.obj";
 
-  // const std::string model_path = "/home/francois/Documents/RayTracing/models/San_Miguel/san-miguel.obj";
-  // const std::string out_ply_path = "/home/francois/Documents/RayTracing/CWBVH/build_make/ply_files/san_miguel.ply";
+  const std::string model_name = "san-miguel";
+  const std::string obj_path_addition = "San_Miguel/san-miguel.obj";
 
   const std::string out_ply_path = ply_base_path + model_name + ".ply";
   const std::string model_path = model_base_path + obj_path_addition;
@@ -31,11 +31,12 @@ int main()
   rg.loadModelOBJ(model_path);
   printf("Done loading obj\n");
   
-  rg.generateObjectRays(100);
+  rg.generateObjectRays(500000);
   printf("Done generating rays\n");
 
   rg.saveRaysToFile(ray_base_path, model_name);
-  rg.readRaysFromFile(ray_base_path + "teapot_100_28_9.ray_file", 100);
+  //rg.readRaysFromFile(ray_base_path + "teapot_100_28_9.ray_file", 100);
+  
   // rg.fillWithListOfRays(); // fill custom list of rays for debugging
   
   rg.uploadRaysToGPU();
